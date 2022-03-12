7efa5e93fa6698c205c9a0dcc77a39a1a53c5b61
noahmank <39310260+noahmank@users.noreply.github.com>
2022-03-11 21:03:33 -0500

    Add commented code for autocorrelation test

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,20 @@ public class Main {
     public static void main(String[] args) {
         Simulator simulator = new Simulator();
         simulator.simulate();
+
+//        RNGenerator generator = new RNGenerator(420);
+//        double[] rands = new double[49];
+//        for(int i = 0; i < rands.length; i++) {
+//            rands[i] = generator.getNext();
+//        }
+//        double sum = 0;
+//        for(int i = 1; i < rands.length; i++) {
+//            sum += rands[i] * rands[i-1];
+//        }
+//        double pim = (sum/49) - 0.25;
+//        System.out.println("Pim: " + pim);
+//        double sigma = Math.sqrt(13*48 + 7) / (12*(49));
+//        System.out.println("Sigma Pim: " + sigma);
+//        System.out.println("Z0: " + pim/sigma);
     }
 }
